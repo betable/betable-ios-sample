4a9fb45889f373db9b65c6d2c69f3c8fb391b7ad
Tony Hauber <thauber@Tonys-MacBook-Air.local>
2012-07-19 01:34:28 -0700

    Initial Commit

---
+++ TestApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestApp
+//
+//  Created by Tony Hauber on 7/19/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TestAppTests/TestAppTests.h
@@ -0,0 +1,13 @@
+//
+//  TestAppTests.h
+//  TestAppTests
+//
+//  Created by Tony Hauber on 7/19/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface TestAppTests : SenTestCase
+
+@end
